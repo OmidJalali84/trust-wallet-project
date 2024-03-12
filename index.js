a2940b29de3cca2dab52f6e4aac8e43348e5c108
omid <omid.jalali.773@gmail.com>
2024-03-12 05:22:43 +0330

    generated css

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-//0x994AC9D988ef1718b5930F658F9eCf439Ea4Bc91
-//0xd64CDF9aa4E0EBc80236b486226ED711B3E961a4
-
 import { abi, contractAddress } from "./usdt-contract.js"
-//import { Web3 } from "web3"
-import { ethers } from "./ethers.js"
-//import Web3 from "https://cdn.esm.sh/v58/web3@1.6.1/es2021/web3.js"
 
 const web3 = new Web3(window.trustwallet)
 
@@ -18,11 +12,13 @@ async function connectToTrustWallet() {
                 window.trustwallet.selectedAddress !== undefined
             ) {
                 changeAddress()
+                getBalance()
                 console.log("Already connected with address:", window.trustwallet.selectedAddress)
             } else {
                 // Request account access if needed
                 await window.trustwallet.request({ method: "eth_requestAccounts" })
                 changeAddress()
+                getBalance()
                 console.log("Connected to Trust Wallet!", window.trustwallet.selectedAddress)
             }
         } else {
@@ -33,25 +29,33 @@ async function connectToTrustWallet() {
     }
 }
 
+console.log(window.trustwallet.selectedAddress)
+
+if (
+    window.trustwallet.selectedAddress !== null &&
+    window.trustwallet.selectedAddress !== undefined
+) {
+    changeAddress()
+    getBalance()
+    console.log("Already connected with address:", window.trustwallet.selectedAddress)
+} else {
+    const connectButton = document.getElementById("connectButton")
+    connectButton.addEventListener("click", () => {
+        connectToTrustWallet()
+        getBalance()
+    })
+}
+
 async function getBalance() {
-    //console.log(window.trustwallet.selectedAddress)
-    // const contract = new ethers.Contract(contractAddress, abi, )
-    // const balance = contract.balanceOf(window.trustwallet.selectedAddress)
     const tokenContract = await new web3.eth.Contract(abi, contractAddress)
     const balance = await tokenContract.methods
         .balanceOf(window.trustwallet.selectedAddress)
         .call()
 
-    document.getElementById("balance").innerHTML = `your balance is: ${balance}`
+    document.getElementById("balance").innerHTML = `your balance is: ${balance} USDT`
 }
 
 function changeAddress() {
     document.getElementById("address").innerHTML =
         `You have connected with ${window.trustwallet.selectedAddress}`
 }
-
-const connectButton = document.getElementById("connectButton")
-connectButton.addEventListener("click", () => {
-    connectToTrustWallet()
-    getBalance()
-})
